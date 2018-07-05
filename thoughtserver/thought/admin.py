f39ed78238110142c2b5bb6a85b4d4522dc4b254
刘运强 <liuyunqiang@yunjinkj.com>
2018-07-05 19:08:18 +0800

    注册models

--- thoughtserver/thought/admin.py
+++ thoughtserver/thought/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from thought.models import User,Thought,DiscussTwo,DiscussOne,SupportDisone,SupportThought,Developer
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Thought)
+admin.site.register(DiscussTwo)
+admin.site.register(DiscussOne)
+admin.site.register(SupportDisone)
+admin.site.register(SupportThought)
+admin.site.register(Developer)
