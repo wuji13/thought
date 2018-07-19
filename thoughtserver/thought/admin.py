e30796a196a3fec0c3d888d0c1725988c8023c49
刘运强 <liuyunqiang@yunjinkj.com>
2018-07-19 14:11:33 +0800

    更新admin的显示和编码格式

--- thoughtserver/thought/admin.py
+++ thoughtserver/thought/admin.py
@@ -1,10 +1,40 @@
+# -*- coding:utf-8 -*-
+
 from django.contrib import admin
-from thought.models import User,Thought,DiscussTwo,DiscussOne,SupportDisone,SupportThought,Developer
+from thought.models import User,Thought,DiscussTwo,DiscussOne,SupportDisone,SupportThought,Developer,WeightFactor
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Thought)
-admin.site.register(DiscussTwo)
-admin.site.register(DiscussOne)
-admin.site.register(SupportDisone)
-admin.site.register(SupportThought)
-admin.site.register(Developer)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id','name','wxId','createTime','time')
+
+class ThoughtAdmin(admin.ModelAdmin):
+    list_display = ('id','auther','createTime','content')
+
+    search_fields = ('aither','id','content')
+
+class DiscussTwoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'auther', 'createTime')
+
+class DiscussOneAdmin(admin.ModelAdmin):
+    list_display = ('id', 'auther', 'createTime')
+
+class SupportDisoneAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createTime')
+
+class SupportThoughtAdmin(admin.ModelAdmin):
+    list_display = ('id', 'createTime')
+
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+class WeightFactorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+admin.site.register(User,UserAdmin)
+admin.site.register(Thought,ThoughtAdmin)
+admin.site.register(DiscussTwo,DiscussTwoAdmin)
+admin.site.register(DiscussOne,DiscussOneAdmin)
+admin.site.register(SupportDisone,SupportDisoneAdmin)
+admin.site.register(SupportThought,SupportThoughtAdmin)
+admin.site.register(Developer,DeveloperAdmin)
+admin.site.register(WeightFactor,WeightFactorAdmin)
--- thoughtserver/thought/views.py
+++ thoughtserver/thought/views.py
@@ -1,3 +1,6 @@
+# -*- coding:utf-8 -*-
+
+
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
 import requests
