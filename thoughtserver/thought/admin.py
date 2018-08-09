1d5f556d01e36674ae073dac58104f1820f3dcb3
刘运强 <liuyunqiang@yunjinkj.com>
2018-08-09 15:01:05 +0800

    admin注册

--- thoughtserver/thought/admin.py
+++ thoughtserver/thought/admin.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from django.contrib import admin
-from thought.models import User,Thought,DiscussTwo,DiscussOne,SupportDisone,SupportThought,Developer,WeightFactor
+from thought.models import User,Thought,DiscussTwo,DiscussOne,SupportDisone,SupportThought,Developer,WeightFactor,Show
 # Register your models here.
 
 class UserAdmin(admin.ModelAdmin):
@@ -30,6 +30,10 @@ class DeveloperAdmin(admin.ModelAdmin):
 class WeightFactorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
 admin.site.register(User,UserAdmin)
 admin.site.register(Thought,ThoughtAdmin)
 admin.site.register(DiscussTwo,DiscussTwoAdmin)
@@ -38,3 +42,4 @@ admin.site.register(SupportDisone,SupportDisoneAdmin)
 admin.site.register(SupportThought,SupportThoughtAdmin)
 admin.site.register(Developer,DeveloperAdmin)
 admin.site.register(WeightFactor,WeightFactorAdmin)
+admin.site.register(Show,ShowAdmin)
